027b62eab1ec91a523743125317bb7f1f4809422
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-19 11:14:09 -0500

    Chnages file format

--- tests/week_one/integer_multiplication/test_integer_multiplication.py
+++ tests/week_one/integer_multiplication/test_integer_multiplication.py
@@ -6,9 +6,10 @@ from algos.divide_conquer_sort_search_random.week_one.integer_multiplication imp
 tests_integer_multiplication = [(),
                                 ]
 
-@pytest.mark.skip(reason="Just a test")
-@pytest.mark.parametrize("zero_padding, answer", tests_initialize_row_results)
-def test_initialize_row_results(zero_padding, answer):
-    assert initialize_row_results(zero_padding) == answer
+
+# @pytest.mark.skip(reason="Just a test")
+# @pytest.mark.parametrize("zero_padding, answer", tests_integer_multiplication)
+# def test_initialize_row_results(zero_padding, answer):
+#     assert grade_school_integer_multiplication(zero_padding) == answer
 
 
